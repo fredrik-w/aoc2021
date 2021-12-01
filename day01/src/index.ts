97e26aef61d160e4f4b3afb8daf92a0cc68a6b1f
Fredrik Wendel <fredrik.wendel@cygni.se>
2021-12-01 21:23:36 +0100

    feat(day01): solve part2

--- day01/src/index.ts
+++ day01/src/index.ts
@@ -13,14 +13,27 @@ export const part1 = (lines: number[]): number => {
   }, 0);
 };
 
+export const part2 = (lines: number[]): number => {
+  const sliding_window = lines.map((val, idx, arr) => {
+    if (idx + 2 >= arr.length) {
+      return -1;
+    }
+    return val + arr[idx + 1] + arr[idx + 2];
+  });
+
+  return part1(sliding_window);
+}
+
 const part = process.env.part || 'part1';
+const numbers = readFile('input.txt');
+
 
 if (require.main === module) {
   let result: unknown;
   if (part === 'part1') {
-    result = part1(readFile('input.txt'));
+    result = part1(numbers);
   } else {
-    result = -1
+    result = part2(numbers)
   }
   console.log(result);
 }
