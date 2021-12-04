5362cbe1cbe8921f589d5531563c9946039b4452
Fredrik Wendel <fredrik.wendel@cygni.se>
2021-12-04 13:57:08 +0100

    chore(day03): reduce footprint

--- day03/src/index.ts
+++ day03/src/index.ts
@@ -1,10 +1,6 @@
 import * as fs from 'fs';
 
-export const readFile = (filename: string) => fs.readFileSync(filename).toString().trim().split('\n');
-
-function toBitList(lines: string[]): number[][] {
-  return lines.map(s => new Array<number>(...(s.split('').map(v => parseInt(v)))));
-}
+export const readFile = (filename: string) => fs.readFileSync(filename).toString().trim().split('\n').map(s => new Array<number>(...(s.split('').map(v => parseInt(v)))));
 
 function occurrences(bits: number[][]): number[] {
   const counter: number[] = new Array(bits[0].length).fill(0);
@@ -12,26 +8,22 @@ function occurrences(bits: number[][]): number[] {
   return counter;
 }
 
-export const part1 = (lines: string[]): number => {
-  const counter = occurrences(toBitList(lines));
-  const gamma = counter.map(v => v > lines.length / 2 ? 1 : 0).join('');
+export const part1 = (bits: number[][]): number => {
+  const counter = occurrences(bits);
+  const gamma = counter.map(v => v > bits.length / 2 ? 1 : 0).join('');
   const epsilon = gamma.replace(/\d/g, (s) => (parseInt(s) ^ 1).toString())
   return parseInt(gamma, 2) * parseInt(epsilon, 2);
 };
 
-export const part2 = (lines: string[]): number => {
-  const filter = (bits: number[][], condition: (x: number, y: number) => number) => {
-    let filteredData: number[][] = bits;
-    for (let i = 0; i < bits[0].length && filteredData.length > 1; i++) {
+export const part2 = (bits: number[][]): number => {
+  const filter = (b: number[][], condition: (x: number, y: number) => number) => {
+    let filteredData: number[][] = b;
+    for (let i = 0; i < b[0].length && filteredData.length > 1; i++) {
       filteredData = filteredData.filter((v) => v[i] === condition(occurrences(filteredData)[i], filteredData.length));
     }
     return filteredData[0];
   }
-  const bits = toBitList(lines);
-  const oxygen = filter(bits, (x, y) => x >= y / 2 ? 1 : 0);
-  const scrubber = filter(bits, (x, y) => x < y / 2 ? 1 : 0);
-
-  return parseInt(oxygen.join(''), 2) * parseInt(scrubber.join(''), 2);
+  return parseInt(filter(bits, (x, y) => x >= y / 2 ? 1 : 0).join(''), 2) * parseInt(filter(bits, (x, y) => x < y / 2 ? 1 : 0).join(''), 2);
 }
 
 require.main === module && console.log((process.env.part === 'part2' ? part2 : part1)(readFile('input.txt')));
