b44e001ae824e7da4c21cd282047e4f469d795eb
Fredrik Wendel <fredrik.wendel@cygni.se>
2021-12-05 11:55:26 +0100

    chore(day04): neater solution

--- day04/src/index.ts
+++ day04/src/index.ts
@@ -2,76 +2,74 @@ import fs from 'fs';
 
 export const readFile = (filename: string) => fs.readFileSync(filename).toString().trim().split('\n');
 
-const getNumbersAndBoards = (lines: string[]) => {
-  const line = (input: string): (string | number)[][] => input.split(/\s+/).filter(k => k.length).map(k => [k, 0]);
+class Board {
+  private marked: number[];
+
+  constructor(private numbers: string[]) {
+    this.marked = new Array(numbers.length).fill(0);
+  }
+
+  public markNumber(number: string) {
+    const idx = this.numbers.findIndex(val => val === number);
+    idx !== -1 && (this.marked[idx] = 1);
+  }
+
+  public winningLine(line: number): boolean {
+    return this.marked[line] + this.marked[line + 1] + this.marked[line + 2] + this.marked[line + 3] + this.marked[line + 4] === 5;
+  }
+
+  public winningColumn(col: number): boolean {
+    return this.marked[col] + this.marked[col + 5] + this.marked[col + 10] + this.marked[col + 15] + this.marked[col + 20] === 5;
+  }
+
+  public hasWon(): boolean {
+    return this.winningLine(0) || this.winningLine(5) || this.winningLine(10) || this.winningLine(15) || this.winningLine(20) || this.winningColumn(0) || this.winningColumn(1) || this.winningColumn(2) || this.winningColumn(3) || this.winningColumn(4);
+  }
+
+  public sumOfUnchecked(): number {
+    return this.numbers.filter((v, idx) => this.marked[idx] === 0).map(s => parseInt(s)).reduce((sum, num) => sum + num, 0);
+  }
+}
+
+const getNumbersAndBoards = (lines: string[]): { numbers: string[], boards: Board[] } => {
+  const line = (input: string): (string)[] => input.split(/\s+/).filter(k => k.length);
   const numbers: string[] = lines[0].split(',');
 
-  let boards: (string | number)[][][][] = [];
+  let boards: Board[] = [];
   for (let i = 2; i < lines.length; i += 6) {
-    boards.push([line(lines[i]), line(lines[i + 1]), line(lines[i + 2]), line(lines[i + 3]), line(lines[i + 4])]);
+    boards.push(new Board([...line(lines[i]), ...line(lines[i + 1]), ...line(lines[i + 2]), ...line(lines[i + 3]), ...line(lines[i + 4])]));
   }
 
   return {numbers, boards};
 }
 
-const markNumber = (boards: (string | number)[][][][], number: string): boolean => {
-  boards.forEach(board => board.forEach(line => line.every(square => {
-    if (square[0] !== number) {
-      return true;
+const drawAndMark = (lines: string[], breakCondition: (x: Array<unknown>, y: Array<unknown>) => boolean): { winners: Board[], lastNumber: number } => {
+  let {numbers, boards} = getNumbersAndBoards(lines);
+  let winners: Board[] = [];
+  let lastNumber: string = '-1';
+  numbers.every(n => {
+    lastNumber = n;
+    for (let i = 0; i < boards.length; i++) {
+      if (!winners.includes(boards[i])) {
+        boards[i].markNumber(n);
+        if (boards[i].hasWon()) {
+          winners.push(boards[i]);
+        }
+      }
     }
-    square[1] = 1;
-  })));
-  return true;
-}
-
-const winningLine = (line: (string | number)[][]): boolean => {
-  return (line[0][1] as number) + (line[1][1] as number) + (line[2][1] as number) + (line[3][1] as number) + (line[4][1] as number) === 5
-}
-
-const winningColumn = (board: (string | number)[][][], col: number): boolean => {
-  return (board[0][col][1] as number) + (board[1][col][1] as number) + (board[2][col][1] as number) + (board[3][col][1] as number) + (board[4][col][1] as number) === 5;
-}
-
-const boardHasWon = (board: (string | number)[][][]): boolean => {
-  return winningLine(board[0]) || winningLine(board[1]) || winningLine(board[2]) || winningLine(board[3]) || winningLine(board[4]) || winningColumn(board, 0) || winningColumn(board, 1) || winningColumn(board, 2) || winningColumn(board, 3) || winningColumn(board, 4);
+    return breakCondition(winners, boards);
+  });
+  return {winners, lastNumber: parseInt(lastNumber)};
 }
 
 export const part1 = (lines: string[]): number => {
-  const {numbers, boards} = getNumbersAndBoards(lines);
-  let winningBoard: (string | number)[][][] = [];
-  let lastNumber: number = 0;
-  numbers.every(n => {
-    markNumber(boards, n);
-    for (let i = 0; i < boards.length; i++) {
-      if (boardHasWon(boards[i])) {
-        winningBoard = boards[i];
-        lastNumber = parseInt(n);
-        break;
-      }
-    }
-    return !winningBoard.length;
-  });
-  const unchecked = winningBoard.flatMap(line => line.filter(square => square[1] === 0).flatMap(square => square[0])).map(v => parseInt(v as string)).reduce((sum, v) => sum + v);
-  return unchecked * lastNumber;
+  const {winners, lastNumber} = drawAndMark(lines, (x, y) => x.length === 0);
+  return winners[0].sumOfUnchecked() * lastNumber;
 }
 
 export const part2 = (lines: string[]): number => {
-  let {numbers, boards} = getNumbersAndBoards(lines);
-  let winningBoards: number[] = [];
-  let lastNumber: number = 0;
-  numbers.every(n => {
-    markNumber(boards, n);
-    for (let i = 0; i < boards.length; i++) {
-      if (!winningBoards.includes(i) && boardHasWon(boards[i])) {
-        winningBoards.push(i);
-      }
-    }
-    lastNumber = parseInt(n);
-    return winningBoards.length !== boards.length;
-  });
-  const looser = boards.filter((v, idx) => idx === winningBoards[winningBoards.length-1])[0];
-  const unchecked = looser.flatMap(line => line.filter(square => square[1] === 0).flatMap(square => square[0])).map(v => parseInt(v as string)).reduce((sum, v) => sum + v);
-  return unchecked * lastNumber;
+  const {winners, lastNumber} = drawAndMark(lines, (x, y) => x.length !== y.length);
+  return winners[winners.length - 1].sumOfUnchecked() * lastNumber;
 }
 
 require.main === module && console.log((process.env.part === 'part2' ? part2 : part1)(readFile('input.txt')));
