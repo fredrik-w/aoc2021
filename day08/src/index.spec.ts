b4663bc13c1186d8777ea10ae06868cc71773526
Fredrik Wendel <fredrik.wendel@cygni.se>
2021-12-08 23:50:02 +0100

    feat(day08): solve part2

--- day08/src/index.spec.ts
+++ day08/src/index.spec.ts
@@ -6,5 +6,5 @@ it('AoC 2021: day08', () => {
   expect(part1(readFile('example-input.txt')), 'part1: example-input.txt').to.equal(26);
   expect(part1(readFile('input.txt')), 'part1: input.txt').to.equal(239);
   expect(part2(readFile('example-input.txt')), 'part2: example-input.txt').to.equal(61229);
-  expect(part2(readFile('input.txt')), 'part2: example.txt').to.equal(-1);
+  expect(part2(readFile('input.txt')), 'part2: example.txt').to.equal(946346);
 });
