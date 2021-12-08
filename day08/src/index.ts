42193bdcf810ecbe1e05dd3b847f8b8bb575a7d3
Fredrik Wendel <fredrik.wendel@cygni.se>
2021-12-08 21:21:59 +0100

    chore(day08): optimize part1

--- day08/src/index.ts
+++ day08/src/index.ts
@@ -1,43 +1,25 @@
 import fs from 'fs';
 
 export const readFile = (filename: string) => fs.readFileSync(filename).toString().trim().split('\n');
-const TOP = 'a';
-const TOP_LEFT = 'b';
-const TOP_RIGHT = 'c';
-const MIDDLE = 'd';
-const BOTTOM_LEFT = 'e';
-const BOTTOM = 'f';
-const BOTTOM_RIGHT = 'g';
 
-const digitsSegments = new Map<number, string[]>([
-  [0, [TOP, TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM, BOTTOM_RIGHT]],
-  [1, [TOP_RIGHT, BOTTOM_RIGHT]],
-  [2, [TOP, TOP_RIGHT, MIDDLE, BOTTOM_LEFT, BOTTOM_RIGHT]],
-  [3, [TOP, TOP_RIGHT, MIDDLE, BOTTOM, BOTTOM_RIGHT]],
-  [4, [TOP_LEFT, TOP_RIGHT, MIDDLE, BOTTOM]],
-  [5, [TOP, TOP_LEFT, MIDDLE, BOTTOM, BOTTOM_RIGHT]],
-  [6, [TOP, TOP_LEFT, MIDDLE, BOTTOM_LEFT, BOTTOM, BOTTOM_RIGHT]],
-  [7, [TOP, TOP_RIGHT, BOTTOM]],
-  [8, [TOP, TOP_LEFT, TOP_RIGHT, MIDDLE, BOTTOM_LEFT, BOTTOM, BOTTOM_RIGHT]],
-  [9, [TOP, TOP_LEFT, TOP_RIGHT, MIDDLE, BOTTOM, BOTTOM_RIGHT]],
-]);
+
+const original = ['abcefg', 'cf', 'acdeg', 'acdfg', 'bcdf', 'abdfg', 'abdefg', 'acf', 'abcdefg', 'abcdfg'];
+const originalMapping = original.map(v => ({wire: v, segment: v}));
 
 const analyzeOccurrenceByLength = (scrambled: string[]): number => {
   const byLength = (length: number) => scrambled.filter(wires => wires.length === length).length;
-  return byLength(digitsSegments.get(1)!.length) +
-     byLength(digitsSegments.get(4)!.length) +
-     byLength(digitsSegments.get(7)!.length)+
-     byLength(digitsSegments.get(8)!.length);
+  return byLength(original[1].length) + byLength(original[4].length) + byLength(original[7]!.length) + byLength(original[8]!.length);
 }
 
 export const part1 = (lines: string[]): number => {
   return lines.map(l => {
-    const [ , scrambledSegments] = l.split(' | ');
+    const [, scrambledSegments] = l.split(' | ');
     return analyzeOccurrenceByLength(scrambledSegments.split(' '))
   }).reduce((sum, count) => sum + count);
 }
 
 export const part2 = (lines: string[]): number => {
+  //lines.map()
   /*
   lines.forEach(l => {
     const { scrambledWires, scrambledSegments } = l.split(' | ');
