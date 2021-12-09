abb9bd13280b206594f0eb8d167adf4a2c87dbdd
Fredrik Wendel <fredrik.wendel@cygni.se>
2021-12-09 08:45:27 +0100

    chore(day08): reduce footprint

--- day08/src/index.ts
+++ day08/src/index.ts
@@ -14,57 +14,29 @@ const filterByLength = (input: string[], length: number) => input.filter(v => v.
 const containsAll = (input: string[], chars: string[]): string => input.filter(s => chars.every(c => s.includes(c)))[0];
 const removeAll = (input: string, chars: string[]): string => input.split('').filter(c => !chars.includes(c)).join('');
 
-const mapAllWires = (wires: string[]) => {
-  let mapping = {
-    top: '',
-    top_left: '',
-    top_right: '',
-    middle: '',
-    bottom_left: '',
-    bottom_right: '',
-    bottom: ''
-  }
-  const one = findByLength(wires, 2)!;
-  const four = findByLength(wires, 4)!;
-  const seven = findByLength(wires, 3)!;
-  const eight = findByLength(wires, 7)!;
-  const nine = containsAll(filterByLength(wires, 6), four.split(''));
-  mapping.top = removeAll(findByLength(wires, 3), findByLength(wires, 2).split(''));
-  const three = containsAll(filterByLength(wires, 5), [...one.split(''), mapping.top]);
-  mapping.bottom = removeAll(three, [...seven.split(''), ...four.split('')]);
-  mapping.bottom_left = removeAll(eight, nine.split(''));
-  mapping.middle = removeAll(three, [...one.split(''), mapping.bottom, mapping.top]);
-  mapping.top_left = removeAll(four, [...one.split(''), mapping.middle]);
-  const six = containsAll(filterByLength(wires, 6), [mapping.top, mapping.top_left, mapping.middle, mapping.bottom_left, mapping.bottom]);
-  mapping.bottom_right = removeAll(six, [mapping.top, mapping.top_left, mapping.middle, mapping.bottom_left, mapping.bottom]);
-  mapping.top_right = removeAll('abcdefg', [mapping.top, mapping.top_left, mapping.middle, mapping.bottom_left, mapping.bottom, mapping.bottom_right]);
-  return mapping;
-}
+const sortString = (s: string): string => s.split('').sort((a, b ) => a > b ? 1 : -1).join('');
 
-function generateSegmentMap(wires: {[key in string]: string}, segments: string[]): string[] {
-  return [
-    containsAll(segments.filter(s => s.length === 6), [wires.top, wires.top_left, wires.top_right, wires.bottom_left, wires.bottom_right, wires.bottom]),
-    containsAll(segments.filter(s => s.length === 2), [wires.top_right, wires.bottom_right]),
-    containsAll(segments.filter(s => s.length === 5), [wires.top, wires.top_right, wires.middle, wires.bottom_left, wires.bottom]),
-    containsAll(segments.filter(s => s.length === 5), [wires.top, wires.top_right, wires.middle, wires.bottom_right, wires.bottom]),
-    containsAll(segments.filter(s => s.length === 4), [wires.top_left, wires.top_right, wires.middle, wires.bottom_right]),
-    containsAll(segments.filter(s => s.length === 5), [wires.top, wires.top_left, wires.middle, wires.bottom_right, wires.bottom]),
-    containsAll(segments.filter(s => s.length === 6), [wires.top, wires.top_left, wires.middle, wires.bottom_right, wires.bottom_left, wires.bottom]),
-    containsAll(segments.filter(s => s.length === 3), [wires.top, wires.top_right, wires.bottom_right]),
-    containsAll(segments.filter(s => s.length === 7), [wires.top, wires.top_left, wires.top_right, wires.middle, wires.bottom_left, wires.bottom_right, wires.bottom]),
-    containsAll(segments.filter(s => s.length === 6), [wires.top, wires.top_left, wires.top_right, wires.middle, wires.bottom_right, wires.bottom]),
-  ];
+const mapWiresToDigits = (wires: string[]) => {
+  let map: string[] = new Array(10);
+  map[1] = findByLength(wires, 2)!;
+  map[3] = containsAll(filterByLength(wires, 5), [...map[1].split('')]);
+  map[4] = findByLength(wires, 4)!;
+  map[5] = containsAll(filterByLength(wires, 5), removeAll(map[4], map[1].split('')).split(''));
+  map[2] = filterByLength(wires, 5).filter(s => s !== map[3] && s !== map[5])[0];
+  map[7] = findByLength(wires, 3)!;
+  map[8] = findByLength(wires, 7)!;
+  map[9] = containsAll(filterByLength(wires, 6), map[4].split(''));
+  map[0] = containsAll(filterByLength(wires, 6).filter(w => w !== map[9]), map[1].split(''));
+  map = map.map(d => sortString(d));
+  map[6] = wires.filter(w => !map.includes(sortString(w)))[0];
+  return map;
 }
 
 export const part2 = (lines: string[]): number => {
   return lines.map(line => {
-    const [wires, segments] = line.split(' | ').map(s => s.split( ' '));
-    const wiresMap = mapAllWires(wires);
-
-    const sortedSegments = segments.map(s => s.split('').sort((a, b) => a > b ? 1 : -1).join(''));
-    const segmentsMap = generateSegmentMap(wiresMap, sortedSegments);
-
-    return +sortedSegments.map(s => segmentsMap.findIndex(m => m === s)).join('');
+    const [wires, segments] = line.split(' | ').map(s => s.split(' '));
+    const wireMap = mapWiresToDigits(wires);
+    return +segments.map(s => sortString(s)).map(s => wireMap.indexOf(s)).join('');
   }).reduce((sum, val) => sum + val);
 }
 
