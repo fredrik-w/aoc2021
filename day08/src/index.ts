79aac26366eca93fb36cf5c54adb92c61599682b
Fredrik Wendel <fredrik.wendel@cygni.se>
2021-12-09 20:27:33 +0100

    chore(day08): reduce footprint

--- day08/src/index.ts
+++ day08/src/index.ts
@@ -9,24 +9,22 @@ export const part1 = (lines: string[]): number => {
   }).reduce((sum, count) => sum + count);
 }
 
-const findByLength = (input: string[], length: number) => input.find(v => v.length === length)!;
 const filterByLength = (input: string[], length: number) => input.filter(v => v.length === length);
-const containsAll = (input: string[], chars: string[]): string => input.filter(s => chars.every(c => s.includes(c)))[0];
-const removeAll = (input: string, chars: string[]): string => input.split('').filter(c => !chars.includes(c)).join('');
-
+const containsAllChars = (input: string[], chars: string[]): string => input.filter(s => chars.every(c => s.includes(c)))[0];
+const removeAllChars = (input: string, chars: string[]): string => input.split('').filter(c => !chars.includes(c)).join('');
 const sortString = (s: string): string => s.split('').sort((a, b ) => a > b ? 1 : -1).join('');
 
 const mapWiresToDigits = (wires: string[]) => {
   let map: string[] = new Array(10);
-  map[1] = findByLength(wires, 2)!;
-  map[3] = containsAll(filterByLength(wires, 5), [...map[1].split('')]);
-  map[4] = findByLength(wires, 4)!;
-  map[5] = containsAll(filterByLength(wires, 5), removeAll(map[4], map[1].split('')).split(''));
+  map[1] = filterByLength(wires, 2)[0];
+  map[3] = containsAllChars(filterByLength(wires, 5), [...map[1].split('')]);
+  map[4] = filterByLength(wires, 4)[0];
+  map[5] = containsAllChars(filterByLength(wires, 5), removeAllChars(map[4], map[1].split('')).split(''));
   map[2] = filterByLength(wires, 5).filter(s => s !== map[3] && s !== map[5])[0];
-  map[7] = findByLength(wires, 3)!;
-  map[8] = findByLength(wires, 7)!;
-  map[9] = containsAll(filterByLength(wires, 6), map[4].split(''));
-  map[0] = containsAll(filterByLength(wires, 6).filter(w => w !== map[9]), map[1].split(''));
+  map[7] = filterByLength(wires, 3)[0];
+  map[8] = filterByLength(wires, 7)[0];
+  map[9] = containsAllChars(filterByLength(wires, 6), map[4].split(''));
+  map[0] = containsAllChars(filterByLength(wires, 6).filter(w => w !== map[9]), map[1].split(''));
   map = map.map(d => sortString(d));
   map[6] = sortString(wires.filter(w => !map.includes(sortString(w)))[0]);
   return map;
