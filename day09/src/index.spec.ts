df72c1e37c1dd543dedd50b7d0d4c4305b015408
Fredrik Wendel <fredrik.wendel@cygni.se>
2021-12-10 13:19:09 +0100

    feat(day09): solve part2

--- day09/src/index.spec.ts
+++ day09/src/index.spec.ts
@@ -2,9 +2,9 @@ import 'mocha';
 import {expect} from 'chai';
 import {part1, part2, readFile} from './index';
 
-it('AoC 2021: day08', () => {
+it('AoC 2021: day09', () => {
   expect(part1(readFile('example-input.txt')), 'part1: example-input.txt').to.equal(15);
   expect(part1(readFile('input.txt')), 'part1: input.txt').to.equal(444);
-  expect(part2(readFile('example-input.txt')), 'part2: example-input.txt').to.equal(42);
-  expect(part2(readFile('input.txt')), 'part2: example.txt').to.equal(42);
+  expect(part2(readFile('example-input.txt')), 'part2: example-input.txt').to.equal(1134);
+  expect(part2(readFile('input.txt')), 'part2: example.txt').to.equal(1168440);
 });
--- day09/src/index.ts
+++ day09/src/index.ts
@@ -2,29 +2,64 @@ import fs from 'fs';
 
 export const readFile = (filename: string) => fs.readFileSync(filename).toString().trim().split('\n');
 
-export const part1 = (lines: string[]): number => {
-  const getAdjacent = (lines: string[], row: number, col: number): number[] => {
-    let values: number[] = [];
-    col > 0 && values.push(+lines[row].charAt(col - 1));
-    row > 0 && values.push(+lines[row - 1].charAt(col));
-    col < lines[row].length-1 && values.push(+lines[row].charAt(col + 1));
-    row < lines.length-1 && values.push(+lines[row + 1].charAt(col));
-    return values;
-  }
+const valueAt = (lines: string[], row: number, col: number): number => +lines[row].charAt(col);
+const isLowPoint = (lines: string[], char: number, adjacent: Array<number[]>): boolean => {
+  let values = adjacent.map(([row, col]) => valueAt(lines, row, col));
+  return !values.includes(char) && Math.min(char, ...values) === char;
+}
 
-  let minPoints: number[] = [];
+const getAdjacent = (lines: string[], row: number, col: number): Array<number[]> => {
+  let adjacent: Array<number[]> = [];
+  col > 0 && adjacent.push([row, col - 1]);
+  row > 0 && adjacent.push([row - 1, col]);
+  col < lines[row].length-1 && adjacent.push([row, col + 1]);
+  row < lines.length-1 && adjacent.push([row + 1, col]);
+  return adjacent;
+}
+
+export const part1 = (lines: string[]): number => {
+  let lowPoints: number[] = [];
   for (let row = 0; row < lines.length; row++) {
     for (let col = 0; col < lines[row].length; col++) {
-      const adjacent = getAdjacent(lines, row, col);
       let char = +lines[row].charAt(col);
-      !adjacent.includes(char) && Math.min(char, ...adjacent) === char && minPoints.push(char + 1);
+      isLowPoint(lines, char, getAdjacent(lines, row, col)) && lowPoints.push(char + 1);
     }
   }
-  return minPoints.reduce((sum, val) => sum + val);
+  return lowPoints.reduce((sum, val) => sum + val);
+}
+
+const exploreBasin = (lines: string[], adjacent: Array<number[]>): Array<number[]> => {
+  const includes = (collection: Array<number[]>, value: number[]): boolean => {
+    return collection.find(([x, y]) => value[0] === x && value[1] === y) !== undefined;
+  }
+
+  let points: Array<number[]> = [];
+  let inspect: Array<number[]> = adjacent;
+  while(inspect.length > 0) {
+    let [row, col] = inspect.pop()!;
+    if (valueAt(lines, row, col) < 9) {
+      !includes(points, [row,col]) && points.push([row, col]);
+      let newPoints = getAdjacent(lines, row, col).filter(point => !includes(points, point) && valueAt(lines, point[0], point[1]) < 9);
+      inspect.push(...newPoints);
+    }
+  }
+  return points;
 }
 
 export const part2 = (lines: string[]): number => {
-  return -1;
+  let basins = []
+
+  for (let row = 0; row < lines.length; row++) {
+    for (let col = 0; col < lines[row].length; col++) {
+      let char = +lines[row].charAt(col);
+      const adjacent = getAdjacent(lines, row, col);
+      if (isLowPoint(lines, char, adjacent)) {
+        basins.push(exploreBasin(lines, adjacent));
+      }
+    }
+  }
+  basins.sort((a, b) => a.length > b.length ? 1 : -1);
+  return basins.slice(-3).map(basin => basin.length).reduce((sum, val) => sum * val);
 }
 
 require.main === module && console.log((process.env.part === 'part2' ? part2 : part1)(readFile('input.txt')));
