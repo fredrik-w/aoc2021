274e2d20cd9b0490e739447848d1505e550e44c9
Fredrik Wendel <fredrik.wendel@cygni.se>
2021-12-11 22:13:27 +0100

    chore(day07): reduce footprint

--- day07/src/index.ts
+++ day07/src/index.ts
@@ -2,9 +2,7 @@ import fs from 'fs';
 
 export const readFile = (filename: string) => fs.readFileSync(filename).toString().trim().split(',').map(s => +s).sort((a, b) => a > b ? 1 : -1);
 
-export const part1 = (crabs: number[]): number => {
-  return crabs.reduce((fuel, position) => fuel + Math.abs(position - crabs[crabs.length / 2]), 0);
-}
+export const part1 = (crabs: number[]): number => crabs.reduce((fuel, position) => fuel + Math.abs(position - crabs[crabs.length / 2]), 0);
 
 export const part2 = (crabs: number[]): number => {
   const fuelCalculator = (desiredPosition: number): number => desiredPosition * (desiredPosition+1) / 2;
