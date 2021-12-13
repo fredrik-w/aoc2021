fd2d4b4ddc50eec199dfac967836b3e0f4a2cb90
Fredrik Wendel <fredrik.wendel@cygni.se>
2021-12-13 14:33:40 +0100

    feat(day13): solve part2.. almost
    Output isn't entierly correct, but it's possible to guess the
    letters.

--- day13/src/index.spec.ts
+++ day13/src/index.spec.ts
@@ -2,9 +2,14 @@ import 'mocha';
 import {expect} from 'chai';
 import {part1, part2, readFile} from './index';
 
-it('AoC 2021: day13', () => {
-  expect(part1(readFile('input.example.txt')), 'part1: input.example.txt').to.equal(17);
-  expect(part1(readFile('input.txt')), 'part1: input.txt').to.equal(759);
-  expect(part2(readFile('input.example.txt')), 'part2: input.example.txt').to.equal(42);
-  expect(part2(readFile('input.txt')), 'part2: input.txt').to.equal(42);
+describe('AoC 2021: day13', () => {
+  it('part1', () => {
+    expect(part1(readFile('input.example.txt')), 'part1: input.example.txt').to.equal(17);
+    expect(part1(readFile('input.txt')), 'part1: input.txt').to.equal(759);
+  });
+
+  it('part2', () => {
+    expect(part2(readFile('input.example.txt')), 'part2: input.example.txt').to.equal(16);
+    expect(part2(readFile('input.txt')), 'part2: input.txt').to.equal(102);
+  });
 });
--- day13/src/index.ts
+++ day13/src/index.ts
@@ -2,32 +2,32 @@ import fs from 'fs';
 
 export const readFile = (filename: string) => fs.readFileSync(filename).toString().trim().split('\n');
 
-const visualize = (paper: number[][]): void => paper.forEach(line => console.log(line.map(v => v === 0 ? '.' : '#').join('')))
+const visualize = (paper: number[][]): void => paper.forEach(line => console.log(line.map(v => v === 0 ? ' ' : '#').join('')))
 
 const parse = (lines: string[]): { paper: number[][], instructions: string[] } => {
   const dots: number[][] = [];
   const instructions: string[] = [];
   lines.filter(v => v.length).forEach(line => line.startsWith('fold') ? instructions.push(line.substring(11)) : dots.push(line.split(',').map(s => +s)));
-  let paper: number[][] = new Array(Math.max(...dots.map(v => v[1]))+1).fill(0).map(_ => new Array(Math.max(...dots.map(v => v[0]))+1).fill(0));
+  let paper: number[][] = new Array(Math.max(...dots.map(v => v[1])) + 1).fill(0).map(_ => new Array(Math.max(...dots.map(v => v[0])) + 1).fill(0));
   dots.forEach(([y, x]) => paper[x][y] = 1);
   return {paper, instructions};
 }
 
-const fold = (paper: number[][], instructions: string[]): number => {
+const fold = (paper: number[][], instructions: string[], printResult: boolean = false): number => {
   instructions.forEach(inst => {
     let [dir, cut] = inst.split('=');
     if (dir === 'y') {
-      paper.slice(+cut).reverse().forEach((v, x) => [...v.entries()].filter(v => v[1] === 1).map(v => v[0]).forEach(y => paper[x][y] = 1));
-      paper = paper.slice(0, +cut);
+      paper.slice(+cut - 1).reverse().forEach((line, x) => line.forEach((v, y) => paper[x][y] += v));
+      paper = paper.slice(0, +cut - 1);
     } else if (dir === 'x') {
       paper.forEach((line, y) => {
-        [...line.slice(+cut).reverse().entries()].filter(v => v[1] === 1).map(v => v[0]).forEach(x => line[x] = 1);
+        line.slice(+cut).reverse().forEach((v, x) => line[x] += v);
         paper[y] = line.slice(0, +cut);
       })
     }
   });
-  return paper.flat().filter(v => v === 1).reduce((sum, val) => sum + val);
-
+  printResult && visualize(paper);
+  return paper.flat().filter(v => v > 0).length;
 }
 
 export const part1 = (lines: string[]): number => {
@@ -35,6 +35,11 @@ export const part1 = (lines: string[]): number => {
   return fold(paper, [instructions[0]]);
 };
 
-export const part2 = (lines: string[]): number => -1;
+export const part2 = (lines: string[]): number => {
+  let {paper, instructions} = parse(lines);
+  return fold(paper, instructions, true);
+}
 
 require.main === module && console.log((process.env.part === 'part2' ? part2 : part1)(readFile('input.txt')));
+
+
