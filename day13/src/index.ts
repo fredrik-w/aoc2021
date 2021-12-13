a1a9c6a25fa4dfa1d39591004285ff340c6eae75
Fredrik Wendel <fredrik.wendel@cygni.se>
2021-12-13 22:37:18 +0100

    chore(day13): part2 adjustments
    This is more correct logic, although it makes the letters harder
    too read. Still isn't right though.

--- day13/src/index.ts
+++ day13/src/index.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 
 export const readFile = (filename: string) => fs.readFileSync(filename).toString().trim().split('\n');
 
-const visualize = (paper: number[][]): void => paper.forEach(line => console.log(line.map(v => v === 0 ? ' ' : '#').join('')))
+const visualize = (paper: number[][]): void => paper.forEach(line => console.log(line.map(v => v === 0 ? '.' : '#').join('')))
 
 const parse = (lines: string[]): { paper: number[][], instructions: string[] } => {
   const dots: number[][] = [];
@@ -17,17 +17,17 @@ const fold = (paper: number[][], instructions: string[], printResult: boolean = 
   instructions.forEach(inst => {
     let [dir, cut] = inst.split('=');
     if (dir === 'y') {
-      paper.slice(+cut - 1).reverse().forEach((line, x) => line.forEach((v, y) => paper[x][y] += v));
-      paper = paper.slice(0, +cut - 1);
+      paper.slice(+cut + 1).reverse().forEach((line, x) => line.forEach((v, y) => paper[x][y] = paper[x][y] || v));
+      paper = paper.slice(0, +cut);
     } else if (dir === 'x') {
       paper.forEach((line, y) => {
-        line.slice(+cut).reverse().forEach((v, x) => line[x] += v);
+        line.slice(+cut).reverse().forEach((v, idx) => line[idx] = line[idx] || v);
         paper[y] = line.slice(0, +cut);
       })
     }
   });
   printResult && visualize(paper);
-  return paper.flat().filter(v => v > 0).length;
+  return paper.flat().filter(v => v === 1).length;
 }
 
 export const part1 = (lines: string[]): number => {
