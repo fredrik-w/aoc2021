bc1db84c7bc323d54044c4e3652e5ee221cc3851
Fredrik Wendel <fredrik.wendel@cygni.se>
2021-12-14 21:01:58 +0100

    feat(day13): reimplement

--- day13/src/index.spec.ts
+++ day13/src/index.spec.ts
@@ -9,7 +9,9 @@ describe('AoC 2021: day13', () => {
   });
 
   it('part2', () => {
-    expect(part2(readFile('input.example.txt')), 'part2: input.example.txt').to.equal(16);
-    expect(part2(readFile('input.txt')), 'part2: input.txt').to.equal(102);
+    expect(part2(readFile('input.example.txt')).length, 'part2: input.example.txt').to.equal(16);
+    expect(part2(readFile('input.example.txt')).output, 'part2: input.example.txt').to.equal("#####\n#...#\n#...#\n#...#\n#####");
+    expect(part2(readFile('input.txt')).length, 'part2: input.txt').to.equal(102);
+    expect(part2(readFile('input.txt')).output, 'part2: input.txt').to.equal("#..#.#\n#..#.#\n####.#\n#..#.#\n#..#.#\n#..#.#\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......\n......");
   });
 });
--- day13/src/index.ts
+++ day13/src/index.ts
@@ -2,44 +2,63 @@ import fs from 'fs';
 
 export const readFile = (filename: string) => fs.readFileSync(filename).toString().trim().split('\n');
 
-const visualize = (paper: number[][]): void => paper.forEach(line => console.log(line.map(v => v === 0 ? '.' : '#').join('')))
+const visualizeDots = (dots: number[][]): string => {
+  const { height, width } = dimensions(dots);
+  let lines: string[] = [];
+  for(let x=0; x<height; x++) {
+    let line: string[] = [];
+    for (let y=0; y<width; y++) {
+      line.push(dots.findIndex(([dotY, dotX]) => x === dotX && y === dotY) !== -1 ? '#' : '.');
+    }
+    lines.push(line.join(''));
+  }
+  return lines.join('\n');
+}
 
-const parse = (lines: string[]): { paper: number[][], instructions: string[] } => {
+const dimensions = (dots: number[][]): { height: number, width: number } => ({ height: Math.max(...dots.map(v => v[0])) + 1, width: Math.max(...dots.map(v => v[1])) + 1 });
+
+const parse = (lines: string[]): { dots: number[][], instructions: string[] } => {
   const dots: number[][] = [];
   const instructions: string[] = [];
   lines.filter(v => v.length).forEach(line => line.startsWith('fold') ? instructions.push(line.substring(11)) : dots.push(line.split(',').map(s => +s)));
-  let paper: number[][] = new Array(Math.max(...dots.map(v => v[1])) + 1).fill(0).map(_ => new Array(Math.max(...dots.map(v => v[0])) + 1).fill(0));
-  dots.forEach(([y, x]) => paper[x][y] = 1);
-  return {paper, instructions};
+  return {dots, instructions};
 }
 
-const fold = (paper: number[][], instructions: string[], printResult: boolean = false): number => {
-  instructions.forEach(inst => {
-    let [dir, cut] = inst.split('=');
-    if (dir === 'y') {
-      paper.slice(+cut + 1).reverse().forEach((line, x) => line.forEach((v, y) => paper[x][y] = paper[x][y] || v));
-      paper = paper.slice(0, +cut);
-    } else if (dir === 'x') {
-      paper.forEach((line, y) => {
-        line.slice(+cut).reverse().forEach((v, idx) => line[idx] = line[idx] || v);
-        paper[y] = line.slice(0, +cut);
-      })
-    }
+const doFold = (dots: number[][], width: number, height: number): number[][] => {
+  const adjust = (pos: number, i: number): number => Math.abs(i - ((2*pos)));
+
+  let newDots: number[][] = [];
+  dots.forEach(dot => {
+    let [y,x] = dot;
+    const newDot = [y > width ? adjust(width, y) : y, x > height ? adjust(height, x) : x];
+    newDots.filter(v => v[0] === newDot[0] && v[1] === newDot[1]).length === 0 && newDots.push(newDot);
   });
-  printResult && visualize(paper);
-  return paper.flat().filter(v => v === 1).length;
+
+  return newDots;
+}
+
+const fold = (dots: number[][], instructions: string[]): number[][] => {
+  let { height: x, width: y } = dimensions(dots);
+
+  instructions.forEach(instruction => {
+    const [dir, pos] = instruction.split('=');
+    dir === 'y' && (y = +pos);
+    dir === 'x' && (x = +pos);
+    dots = doFold(dots, x, y);
+  });
+
+  return dots;
 }
 
 export const part1 = (lines: string[]): number => {
-  let {paper, instructions} = parse(lines);
-  return fold(paper, [instructions[0]]);
+  const {dots, instructions} = parse(lines);
+  return fold(dots, [instructions[0]]).length;
 };
 
-export const part2 = (lines: string[]): number => {
-  let {paper, instructions} = parse(lines);
-  return fold(paper, instructions, true);
+export const part2 = (lines: string[]): { length: number, output: string } => {
+  let {dots, instructions} = parse(lines);
+  const foldedDots = fold(dots, instructions);
+  return { length: foldedDots.length, output: visualizeDots(foldedDots) };
 }
 
 require.main === module && console.log((process.env.part === 'part2' ? part2 : part1)(readFile('input.txt')));
-
-
